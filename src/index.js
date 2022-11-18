66bb32b95606707ea99fac3a233fbb9d09ac9026
paras9sid <sidharthjainp9@gmail.com>
2022-11-18 22:40:46 +0530

    store created new method configureStore @redux/toolkit library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+//package imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { createStore } from 'redux';  configureStroe from @redux/toolkit
+import { configureStore } from '@reduxjs/toolkit';
+
+//file imports
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = configureStore({
+  reducer: {  // new way to mention/declare configureStore reducer
+    movies
+  }
+});
+console.log('store',store);
+console.log('STATE',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
-export default function movies(state = [],action) {
+export default function movies(state = [],action) { // will print in console - state = empty array [] when store.getState() mehtod called in index.js
     if(action.type === 'ADD_MOVIES'){
         return action.movies;
     }
     return state;
 }
+
